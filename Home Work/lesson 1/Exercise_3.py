b0e36a8df0bce2dd74e786a8c7dc2a8f54642810
kns <boamod@163.com>
2020-04-08 16:37:21 +1000

    les 1 exercise 3 new edit

--- Home Work/lesson 1/Exercise_3.py
+++ Home Work/lesson 1/Exercise_3.py
@@ -1,16 +1,35 @@
 # Узнайте у пользователя число n.Найдите сумму
 # чисел n + nn + nnn.Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
+def under_zero(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 while True:
     print('Для выхода напишите выход, для счета введите целое число')
     ask_number = input('Введите целое число:\n')
-    num_one = ask_number
-    num_twice = ask_number * 2
-    num_trinity = ask_number * 3
-    print(num_one, num_twice, num_trinity)
     if ask_number == 'выход': break
-    if ask_number.isdigit():
-        print(int(num_one)+int(num_twice)+int(num_trinity))
+    if ask_number.isdigit() or under_zero(ask_number):
+        if int(ask_number) > 0:
+            num_one = ask_number
+            num_twice = ask_number * 2
+            num_trinity = ask_number * 3
+            print(num_one, num_twice, num_trinity)
+            print(int(num_one) + int(num_twice) + int(num_trinity))
+
+        elif int(ask_number) < 0:
+            num_one = ask_number
+            num_one = ask_number
+            num_twice = ask_number* 2
+            num_trinity = ask_number * 3
+            print(num_one, num_twice, num_trinity)
+            num_twice = int(ask_number) + int(ask_number)
+            num_trinity = int(ask_number) + int(ask_number) + int(ask_number)
+            print(int(num_one) + int(num_twice) + int(num_trinity))
+        elif int(ask_number) == 0:
+            print('0','00','000')
+            print(0)
     else:
         print('Ввведите целое число')
         continue
