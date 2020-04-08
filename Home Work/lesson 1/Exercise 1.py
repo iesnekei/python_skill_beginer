e91a0292d7c7f73bc9143d71980b86b9f0fec47a
kns <boamod@163.com>
2020-04-08 11:32:34 +1000

    les 1 exercise 1

---
+++ Home Work/lesson 1/Exercise 1.py
@@ -0,0 +1,40 @@
+import random
+
+ask_str_list = []
+
+i = 1
+
+while i <= random.randint(1,3):
+    ask_str = input('Введите какое нибудь слово:\n')
+    if ask_str.isdigit():
+        print('Введите слово а не число')
+        continue
+    else:
+        ask_str_list.append(ask_str)
+        i += 1
+
+print(ask_str_list)
+
+ask_int_list = []
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+i = 1
+
+while i <= random.randint(1,3):
+    ask_int = input('Введите какое нибудь число :\n')
+    if ask_int.isdigit:
+        ask_int_list.append(ask_int)
+        i += 1
+    elif isfloat(ask_int):
+        ask_int_list.append(ask_int)
+        i += 1
+    else:
+        print('Введите число')
+        continue
+
+print(ask_int_list)
