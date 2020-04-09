fa37b691c9e85d83e4ccc50b2c2ac8e54431c557
kns <boamod@163.com>
2020-04-10 07:30:55 +1000

    Les 2 Ex 1

---
+++ Home Work/les 2/ex_1.py
@@ -0,0 +1,11 @@
+my_list = [
+           'abc',
+            123,
+            (1,2,3),
+            [1,2,3],
+            {'a':1,'b':2,'c':3},
+            True
+            ]
+
+for obj in my_list:
+    print(type(obj))
