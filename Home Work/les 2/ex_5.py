e2beb614587c3b0ed1044691a58aef62acf7abf1
kns <boamod@163.com>
2020-04-10 09:51:37 +1000

    Les 2 Ex 5

---
+++ Home Work/les 2/ex_5.py
@@ -0,0 +1,14 @@
+my_list = [9, 5, 6, 8, 1]
+
+while True:
+    ask_new_element = input('Write a number :\n ...')
+    if ask_new_element.isdigit():
+        ask_new_element = int(ask_new_element)
+        my_list.append(ask_new_element)
+        my_list.sort()
+        my_list.reverse()
+        print(my_list)
+        break
+    else:
+        print('Please write a NUMBER')
+        continue
