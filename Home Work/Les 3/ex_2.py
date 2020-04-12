a85469df601f4de47bda43da0bd0571108eb5d94
kns <boamod@163.com>
2020-04-13 08:13:22 +1000

    ex_2 : member_info

---
+++ Home Work/Les 3/ex_2.py
@@ -0,0 +1,4 @@
+def member_info(name,surname,birth_day,address,email,phone):
+    print(f'{name} {surname} :\nborn in {birth_day}\nliving address: {address}\nemail:{email}\nphone:{phone}')
+
+member_info('Kensei','Smart','1990-01-01','China,Beijing','kensei@gmail.com','1755356687')
