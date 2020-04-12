d3c4336ef60bc939f3460880e147f5ade6859711
kns <boamod@163.com>
2020-04-13 09:44:08 +1000

    ex_6: capitalize all words in user inputed string

---
+++ Home Work/Les 3/ex_6.py
@@ -0,0 +1,17 @@
+def func(text):
+    text = text[:]
+    text = text[0].capitalize() + text[1:]
+    return text
+print(func('text'))
+print(func('hahah'))
+
+str_to_change = input('Input a string with several word and only small letter:... ')
+
+list_of_str = str_to_change.split(' ')
+b = 0
+for i in list_of_str:
+    a = func(i)
+    list_of_str[b] = a
+    b += 1
+
+print(list_of_str)
