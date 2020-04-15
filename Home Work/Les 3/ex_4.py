2d787d2cebcf0f0ed0b71b63166394f6b7a6733f
kns <boamod@163.com>
2020-04-15 12:29:39 +1000

    simplify ex_4

--- Home Work/Les 3/ex_4.py
+++ Home Work/Les 3/ex_4.py
@@ -1,14 +1,21 @@
 def x_raise_y(x,y):
+    result = x **y
+    print(f'{result:.4f}')
+
+x_raise_y(2.2,-4)
+
+def x_raise_y_2(x,y):
+    if y >= 0:
+        z = x
+        for i in range(0,(y-1)):
+            x *= z
+        return x
     if y < 0:
-        y = abs(y)
-    if y == 0:
-        x = 1
-        a = 1
-    else:
-        a = x
-        for z in range(1,y):
-            a = a * x
+        z = x
+        for i in range(0,(abs(y)-1)):
+            x *= z
+    result = 1 / x
+    return result
 
-    print(a)
 
-x_raise_y(2,4)
+print(f'{x_raise_y_2(2.2,-4):.4f}')
