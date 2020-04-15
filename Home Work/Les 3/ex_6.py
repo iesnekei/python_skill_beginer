9ee1e14546268ee15d387af78dc2b10faa8f3139
kns <boamod@163.com>
2020-04-15 13:09:33 +1000

    simplify ex_6

--- Home Work/Les 3/ex_6.py
+++ Home Work/Les 3/ex_6.py
@@ -8,10 +8,16 @@ print(func('hahah'))
 str_to_change = input('Input a string with several word and only small letter:... ')
 
 list_of_str = str_to_change.split(' ')
+
 b = 0
+
+
 for i in list_of_str:
     a = func(i)
     list_of_str[b] = a
     b += 1
+
+
+list_of_str = ' '.join(list_of_str)
 
 print(list_of_str)
