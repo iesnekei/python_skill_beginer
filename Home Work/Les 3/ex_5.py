4628d14c4e5ccb58ecd3e3afbf0d23ed0b8f9747
kns <boamod@163.com>
2020-04-15 13:03:24 +1000

    simplify ex_5

--- Home Work/Les 3/ex_5.py
+++ Home Work/Les 3/ex_5.py
@@ -1,29 +1,32 @@
-def from_str_to_list_of_number(str):
-    str = str.split(' ')
-    return str
-
 def sum_of_number_in_list(list):
     b = 0
     for i in list:
         if i == '*':
             break
         else:
-            a = int(i)
-            b += a
+            try:
+                a = int(i)
+                b += a
+            except ValueError:
+                print("Don't input letter or more that 1 space between numbers or space in the end!!!\n"
+                      "Or * without space before!!!")
+
     return b
 
 growing_number = 0
 
 while True:
-    ask_numbers_row = input('Please input a row of numbers separating by space! Or input * to exit:...')
+    ask_numbers_row = input('Please input a row of numbers separating by space! Or input * to exit:... ')
+
     if '*' in ask_numbers_row:
-        my_list = from_str_to_list_of_number(ask_numbers_row)
+        my_list = ask_numbers_row.split(' ')
         result = sum_of_number_in_list(my_list)
         growing_number += result
         print(growing_number)
         break
+
     else:
-        my_list = from_str_to_list_of_number(ask_numbers_row)
+        my_list = ask_numbers_row.split(' ')
         result = sum_of_number_in_list(my_list)
         growing_number += result
         print(growing_number)
