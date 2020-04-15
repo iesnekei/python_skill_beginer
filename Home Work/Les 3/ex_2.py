d781bb72dc15e8aac274d20862da48cf53ea88dd
kns <boamod@163.com>
2020-04-15 11:43:51 +1000

    simplify ex_2

--- Home Work/Les 3/ex_2.py
+++ Home Work/Les 3/ex_2.py
@@ -1,4 +1,18 @@
-def member_info(name,surname,birth_day,address,email,phone):
+def member_info(
+                name,
+                surname,
+                birth_day,
+                address,
+                email,
+                phone
+                ):
     print(f'{name} {surname} :\nborn in {birth_day}\nliving address: {address}\nemail:{email}\nphone:{phone}')
 
-member_info('Kensei','Smart','1990-01-01','China,Beijing','kensei@gmail.com','1755356687')
+member_info(
+            surname= 'Kensei',
+            phone= '1755356687',
+            address= 'China,Beijing',
+            birth_day= '1990-01-01',
+            email= 'kensei@gmail.com',
+            name= 'Smart',
+            )
