b03eb696c6a3624a0aef7eed8525eb9bc58db37e
kns <boamod@163.com>
2020-04-15 11:52:34 +1000

    simplify ex_3

--- Home Work/Les 3/ex_3.py
+++ Home Work/Les 3/ex_3.py
@@ -1,48 +1,6 @@
-def isfloat(Value):
-    try:
-        float(Value)
-        return True
-    except ValueError:
-        return False
+def bigest_numbers_of_3(a,b,c):
+    max_1 = max(a,b)
+    max_2 = max(b,c)
+    return max_1 + max_2
 
-def bigest_number_of_3():
-
-    a = input('Input a number!')
-    try:
-        if a.isdigit():
-            a = int(a)
-
-        elif isfloat(a):
-            a = float(a)
-
-    except ValueError:
-        print('Input a number!')
-
-    b = input('Input a number!')
-    try:
-        if b.isdigit():
-            b = int(b)
-
-        elif isfloat(b):
-            b = float(b)
-
-    except ValueError:
-        print('Input a number!')
-
-    c = input('Input a number!')
-    try:
-        if c.isdigit():
-            c = int(c)
-
-        elif isfloat(c):
-            c = float(c)
-
-    except ValueError:
-        print('Input a number!')
-
-    max_number_1 = max(a,b)
-    max_number_2 = max(b,c)
-    sum_of_numb = max_number_2 + max_number_1
-    return sum_of_numb
-
-print(bigest_number_of_3())
+print(bigest_numbers_of_3(1,2,3))
